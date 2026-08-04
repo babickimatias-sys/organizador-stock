f2e256cefeac40fcb76712b22730e9ea06b06059
babickimatias-sys <babickimatias@gmail.com>
2026-08-04 20:56:18 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'organizador-stock-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Los pedidos a fonts.googleapis.com / cdnjs (Tesseract) van directo a la red.
+  if (!event.request.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        const copia = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
